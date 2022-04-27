c49c54bc061383e2107aabcfb0f26db641aed57d
natsmarinho <natsmarinho@gmail.com>
2022-04-27 20:14:04 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,42 @@
+function saida(){
+    let entr = document.getElementById("entrada")
+    entr.style.display = "none"
+      let saida = document.getElementById("greetings")
+    saida.style.display = "flex"
+//nota.innerHTML = `You selected ${rating} out of 5`
+}
+
+function entrada1(){
+    let nota1 = document.getElementById('rate1')
+    nota1 = 1
+    let nota = document.getElementById('rating')
+    nota.innerHTML = `You selected ${nota1} out of 5`
+}
+
+function entrada2(){
+let nota2 = document.getElementById('rate2')
+nota2 = 2
+let nota = document.getElementById('rating')
+nota.innerHTML = `You selected ${nota2} out of 5`
+}
+
+function entrada3(){
+    let nota3 = document.getElementById('rate3')
+    nota3 = 3
+    let nota = document.getElementById('rating')
+    nota.innerHTML = `You selected ${nota3} out of 5`
+}
+
+function entrada4(){
+let nota4 = document.getElementById('rate4')
+nota4 = 4
+let nota = document.getElementById('rating')
+nota.innerHTML = `You selected ${nota4} out of 5`
+}
+
+function entrada5(){
+    let nota5 = document.getElementById('rate5')
+    nota5 = 5
+    let nota = document.getElementById('rating')
+    nota.innerHTML = `You selected ${nota5} out of 5`
+}
